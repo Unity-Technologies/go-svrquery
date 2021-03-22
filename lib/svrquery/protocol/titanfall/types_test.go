836a246866c101e5551a37baa4580586ac705c22
lwaddicor <lewisw@unity3d.com>
2021-03-22 14:00:57 +0000

    feat: Add tf2e-v8 health flag helper functions

---
+++ lib/svrquery/protocol/titanfall/types_test.go
@@ -0,0 +1,65 @@
+package titanfall
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHealthFlags(t *testing.T) {
+	testCases := []struct {
+		input               uint32
+		expNone             bool
+		expPacketLossIn     bool
+		expPacketLossOut    bool
+		expPacketChokedIn   bool
+		expPacketChokedOut  bool
+		expSlowServerFrames bool
+		expHitching         bool
+	}{
+		{
+
+			input:   0,
+			expNone: true,
+		},
+		{
+
+			input:           1 << 0,
+			expPacketLossIn: true,
+		},
+		{
+			input:            1 << 1,
+			expPacketLossOut: true,
+		},
+		{
+			input:             1 << 2,
+			expPacketChokedIn: true,
+		},
+		{
+			input:              1 << 3,
+			expPacketChokedOut: true,
+		},
+		{
+			input:               1 << 4,
+			expSlowServerFrames: true,
+		},
+		{
+			input:       1 << 5,
+			expHitching: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("verify 0b%b", tc.input), func(t *testing.T) {
+			hf := HealthFlags(tc.input)
+			require.Equal(t, tc.expNone, hf.None())
+			require.Equal(t, tc.expPacketLossIn, hf.PacketLossIn())
+			require.Equal(t, tc.expPacketLossOut, hf.PacketLossOut())
+			require.Equal(t, tc.expPacketChokedIn, hf.PacketChokedIn())
+			require.Equal(t, tc.expPacketChokedOut, hf.PacketChokedOut())
+			require.Equal(t, tc.expSlowServerFrames, hf.SlowServerFrames())
+			require.Equal(t, tc.expHitching, hf.Hitching())
+		})
+	}
+}
