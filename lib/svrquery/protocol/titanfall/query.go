ac634797f77fe8d20a0e0bcc5b85ac0732e9a571
lwaddicor <lewisw@unity3d.com>
2021-11-29 19:18:51 +0000

    Simplify the read to just anything and rearange the tag/data/nonce

--- lib/svrquery/protocol/titanfall/query.go
+++ lib/svrquery/protocol/titanfall/query.go
@@ -5,22 +5,22 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"os"
+
 	"github.com/multiplay/go-svrquery/lib/svrquery/common"
 	"github.com/multiplay/go-svrquery/lib/svrquery/protocol"
 	"github.com/netdata/go-orchestrator/module"
-	"io"
-	"os"
 )
 
 var (
 
 	// minLength is the smallest packet we can expect.
-	minLength = 26
-	nonceSize = 12
-	tagSize = 16
+	minLength  = 26
+	nonceSize  = 12
+	tagSize    = 16
 	packetSize = 1200
 )
 
@@ -49,22 +49,40 @@ func encrypt(b []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	c, err := aes.NewCipher([]byte(hex.EncodeToString(keyBytes)))
+	hexKey := []byte(hex.EncodeToString(keyBytes))
+	fmt.Println("key length", len(hexKey))
+	fmt.Println("key", hexKey)
+
+	c, err := aes.NewCipher(hexKey)
 	if err != nil {
 		return nil, err
 	}
 
-    gcm, err := cipher.NewGCM(c)
-    if err != nil {
-        fmt.Println(err)
-    }
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
 
-    nonce := make([]byte, gcm.NonceSize())
-    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-        fmt.Println(err)
-    }
+	fmt.Println("Nonce size", gcm.NonceSize())
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
 
-	return gcm.Seal(nonce, nonce, b, nil), nil
+	fmt.Println("Nonce: ", nonce)
+
+	// This will output in the form CipherTest | Tag and will need rearranging
+	ciperTextAndTag := gcm.Seal(nil, nonce, b, nil)
+	fmt.Println(ciperTextAndTag)
+
+	// Rearange output to nonce | tag | ciphertext
+	newCipherText := nonce
+	newCipherText = append(newCipherText, ciperTextAndTag[len(ciperTextAndTag)-tagSize:]...)
+	newCipherText = append(newCipherText, ciperTextAndTag[:len(ciperTextAndTag)-tagSize]...)
+
+	fmt.Println(newCipherText)
+
+	return newCipherText, nil
 }
 
 func decrypt(b []byte) ([]byte, error) {
@@ -83,20 +101,20 @@ func decrypt(b []byte) ([]byte, error) {
 		return nil, err
 	}
 
-    gcm, err := cipher.NewGCM(c)
-    if err != nil {
-        fmt.Println(err)
-    }
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    if len(b) < gcm.NonceSize() {
-        return nil, fmt.Errorf("incoming bytes smaller than %d", gcm.NonceSize())
-    }
+	if len(b) < gcm.NonceSize() {
+		return nil, fmt.Errorf("incoming bytes smaller than %d", gcm.NonceSize())
+	}
 
-    nonce, b := b[:gcm.NonceSize()], b[gcm.NonceSize():]
-    plaintext, err := gcm.Open(nil, nonce, b, nil)
-    if err != nil {
-        return nil, err
-    }
+	nonce, b := b[:gcm.NonceSize()], b[gcm.NonceSize():]
+	plaintext, err := gcm.Open(nil, nonce, b, nil)
+	if err != nil {
+		return nil, err
+	}
 
 	return plaintext, nil
 }
@@ -119,72 +137,81 @@ func (q *queryer) Query() (protocol.Responser, error) {
 		return nil, err
 	}
 
+	fmt.Printf("Length: %d\n", len(b))
+
 	if _, err := q.c.Write(b); err != nil {
 		return nil, fmt.Errorf("query write: %w", err)
 	}
 
-	n, err := q.c.Read(b)
-	if err != nil {
-		return nil, fmt.Errorf("query read: %w", err)
-	} else if n < minLength {
-		return nil, fmt.Errorf("packet too short (len: %d)", n)
-	}
+	testRead := make([]byte, 16)
+	n, err := q.c.Read(testRead)
+	// Output everything
+	fmt.Println(n, err, testRead)
 
-	b, err = decrypt(b)
-	if err != nil {
-		return nil, err
-	}
-
-	r := common.NewBinaryReader(b[:n], binary.LittleEndian)
-	i := &Info{}
-
-	// Header.
-	if err = r.Read(&i.Header); err != nil {
-		return nil, err
-	} else if i.Command != ServerInfoResponse {
-		return nil, fmt.Errorf("unexpected cmd %x", i.Command)
-	}
-
-	if i.Version > 1 {
-		// InstanceInfo.
-		if err = q.instanceInfo(r, i); err != nil {
-			return nil, err
-		}
-	}
-
-	// BasicInfo.
-	if err = q.basicInfo(r, i); err != nil {
-		return nil, err
-	}
-
-	if i.Version > 4 {
-		// PerformanceInfo.
-		if err = r.Read(&i.PerformanceInfo); err != nil {
-			return nil, err
-		}
-	}
-
-	if i.Version > 2 {
-		if i.Version > 5 {
-			// MatchState and Teams.
-			if err = r.Read(&i.MatchState); err != nil {
-				return nil, err
-			}
-		} else if err = r.Read(&i.MatchState.MatchStateV2); err != nil {
-			return nil, err
-		}
-
-		if err = q.teams(r, i); err != nil {
-			return nil, err
-		}
-	}
-
-	// Clients
-	if err = q.clients(r, i); err != nil {
-		return nil, err
-	}
-
-	return i, nil
+	return nil, fmt.Errorf("debug")
+	//
+	//n, err := q.c.Read(b)
+	//if err != nil {
+	//	return nil, fmt.Errorf("query read: %w", err)
+	//} else if n < minLength {
+	//	return nil, fmt.Errorf("packet too short (len: %d)", n)
+	//}
+	//
+	//b, err = decrypt(b)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//r := common.NewBinaryReader(b[:n], binary.LittleEndian)
+	//i := &Info{}
+	//
+	//// Header.
+	//if err = r.Read(&i.Header); err != nil {
+	//	return nil, err
+	//} else if i.Command != ServerInfoResponse {
+	//	return nil, fmt.Errorf("unexpected cmd %x", i.Command)
+	//}
+	//
+	//if i.Version > 1 {
+	//	// InstanceInfo.
+	//	if err = q.instanceInfo(r, i); err != nil {
+	//		return nil, err
+	//	}
+	//}
+	//
+	//// BasicInfo.
+	//if err = q.basicInfo(r, i); err != nil {
+	//	return nil, err
+	//}
+	//
+	//if i.Version > 4 {
+	//	// PerformanceInfo.
+	//	if err = r.Read(&i.PerformanceInfo); err != nil {
+	//		return nil, err
+	//	}
+	//}
+	//
+	//if i.Version > 2 {
+	//	if i.Version > 5 {
+	//		// MatchState and Teams.
+	//		if err = r.Read(&i.MatchState); err != nil {
+	//			return nil, err
+	//		}
+	//	} else if err = r.Read(&i.MatchState.MatchStateV2); err != nil {
+	//		return nil, err
+	//	}
+	//
+	//	if err = q.teams(r, i); err != nil {
+	//		return nil, err
+	//	}
+	//}
+	//
+	//// Clients
+	//if err = q.clients(r, i); err != nil {
+	//	return nil, err
+	//}
+	//
+	//return i, nil
 }
 
 // instanceInfo decodes the instance information from a response.
